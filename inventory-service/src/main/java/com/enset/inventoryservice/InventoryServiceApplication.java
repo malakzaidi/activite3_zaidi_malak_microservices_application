bec09e00e5590f4cda1c3b2e36a93800156eb807
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-20 20:27:17 +0100

    implement the inventory service

--- inventory-service/src/main/java/com/enset/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/enset/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,13 @@
 package com.enset.inventoryservice;
 
+import com.enset.inventoryservice.entities.Product;
+import com.enset.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +16,32 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner init(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(Product.builder()
+                            .id(UUID.randomUUID().toString())
+                            .name("Computer")
+                            .price(1500)
+                            .quantity(15)
+                            .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("Airpod")
+                    .price(130)
+                    .quantity(20)
+                    .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("Smartphone")
+                    .price(2599)
+                    .quantity(10)
+                    .build());
+            productRepository.findAll().forEach(product ->
+                    System.out.println(product.toString()));
+
+
+
+        };
+    }
 }
