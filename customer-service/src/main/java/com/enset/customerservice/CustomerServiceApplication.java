145ebafab25c1c3122646c2ba915bab08d27ecd9
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-20 17:28:31 +0100

    implement the customer service

--- customer-service/src/main/java/com/enset/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/enset/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,46 @@
 package com.enset.customerservice;
 
+import com.enset.customerservice.entities.Customer;
+import com.enset.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("c1").email("c1@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("c2").email("c2@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("c3").email("c3@gmail.com")
+                    .build());
+            customerRepository.findAll().forEach(c ->
+            {
+                System.out.println("=================");
+                System.out.println(c.getId());
+                System.out.println(c.getName());
+                System.out.println(c.getEmail());
+                System.out.println("=================");
+
+            });
+
+
+
+
+
+        };
     }
 
 }
---
+++ customer-service/src/main/java/com/enset/customerservice/entities/Customer.java
@@ -0,0 +1,20 @@
+package com.enset.customerservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Customer {
+    @Id @GeneratedValue (strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+}
