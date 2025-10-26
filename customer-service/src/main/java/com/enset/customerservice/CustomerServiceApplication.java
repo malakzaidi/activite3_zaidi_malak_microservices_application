302ff61c94bc8b94a3496ebed82d97690e5b1f02
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-26 23:32:09 +0100

    test the configuration updates with actuator

--- customer-service/src/main/java/com/enset/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/enset/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.enset.customerservice;
 
+import com.enset.customerservice.config.CustomerConfigParams;
 import com.enset.customerservice.entities.Customer;
 import com.enset.customerservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
